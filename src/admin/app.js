175c58396d5de2e6cd6a5220fa2e23a39f496347
prismaplatform <contact@theprismaplatform.com>
2024-03-15 14:32:14 +0200

    personalizing Strapi dashboard

---
+++ src/admin/app.js
@@ -0,0 +1,17 @@
+export default {
+  config: {
+    translations: {
+      en: {
+        "app.components.LeftMenu.navbrand.title": "Prisma Solutions",
+        "app.components.LeftMenu.navbrand.workplace": "Admin Dashboard",
+        "Auth.form.welcome.title": "Welcome to Prisma Admin",
+        "Auth.form.welcome.subtitle": "Login to your account",
+        "Settings.profile.form.section.experience.interfaceLanguageHelp":
+          "Preference changes will apply only to you.",
+      },
+    },
+  },
+  bootstrap(app) {
+    console.log(app);
+  },
+};
